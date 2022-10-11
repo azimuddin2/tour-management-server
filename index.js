004270a71a89f8aaac45472266f775155d94d3c0
azimuddin2 <mohammadazimuddin274@gmail.com>
2022-10-11 20:31:06 +0600

    database connection url

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = require('./app');
 
 
 // database connection
-mongoose.connect(process.env.DATABASE_LOCAL, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+mongoose.connect(process.env.DATABASE_LOCAL || '127.0.0.1/tour-management', { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
     console.log(`Database connection is successful`.red.bold);
 }).catch((err) => {
     console.log("Not Connected to Database ERROR! ", err);
