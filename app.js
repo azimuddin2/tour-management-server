cea21f324484cdc34c3b5b32bd81a1cc30ab839a
azimuddin2 <mohammadazimuddin274@gmail.com>
2022-10-11 10:05:29 +0600

    tour management server

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose')
+
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+
+// routes
+const tourRoute = require('./routes/tour.route')
+
+
+app.get('/', (req, res) => {
+    res.send('Route is working! YAY');
+})
+
+app.use('/api/v1/tour', tourRoute)
+
+
+module.exports = app;
